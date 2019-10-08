32a49a87b93ed7a99595a0a2b83ca6ef598950e5
yousef <yousef.hamadah@gmail.com>
2019-10-08 08:59:04 +0300

    Done.

--- Components/CartButton/index.js
+++ Components/CartButton/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
-import { Icon } from "native-base";
+import { Icon, Text } from "native-base";
+import { connect } from "react-redux";
 import { withNavigation } from "react-navigation";
 
 class CartButton extends Component {
   render() {
     return (
-      <Icon
-        onPress={() => this.props.navigation.navigate("CoffeeCart")}
-        name="shoppingcart"
-        type="AntDesign"
-      />
+      <Text style={{ color: "white" }}>
+        {this.props.items.length}
+        <Icon
+          style={{ color: "white" }}
+          onPress={() => this.props.navigation.navigate("CoffeeCart")}
+          name="shoppingcart"
+          type="AntDesign"
+        />
+      </Text>
     );
   }
 }
 
-export default withNavigation(CartButton);
+const mapStateToProps = state => ({
+  items: state.cartReducer.items
+});
+
+export default withNavigation(connect(mapStateToProps)(CartButton));
